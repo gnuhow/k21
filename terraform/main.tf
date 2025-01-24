7ae5d75473eca3bfb96b5f66be8934576cbf56d7
gnuhow <gnuhow@gmail.com>
2025-01-24 16:00:07 -0500

    tf plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.16.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {}
+}
+
+
 resource "azurerm_resource_group" "test" {
   name     = "k21-test"
   location = "US East"
