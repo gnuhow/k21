8019738e00d51e3fcdf7874c1a8502ccf5a55cc7
gnuhow <gnuhow@gmail.com>
2025-02-12 15:10:09 -0500

    nginx

--- container/web/entrypoint.sh
+++ container/web/entrypoint.sh
@@ -1,3 +1,4 @@
 # APP_URL="https://www.example.com"
 # echo "remote URL: ${APP_URL}"
+curl "${APP_URL}/start"
 sed -i "s~__APP_URL__~${APP_URL}~g" /etc/nginx/conf.d/default.conf && echo "custom entrypoint script complete"
