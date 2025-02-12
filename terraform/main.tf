400f9671d65576f45711f9371abf6da215b8ade9
gnuhow <gnuhow@gmail.com>
2025-02-12 16:13:05 -0500

    destroy app environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,33 +79,33 @@ resource "azurerm_user_assigned_identity" "app" {
 }
 
 
-resource "azurerm_role_definition" "app" {
-  name        = var.project_name_long
-  scope       = join("", ["/subscriptions/", var.subscription_id])
-  description = "Used by the k21 container app"
+# resource "azurerm_role_definition" "app" {
+#   name        = var.project_name_long
+#   scope       = join("", ["/subscriptions/", var.subscription_id])
+#   description = "Used by the k21 container app"
 
-  permissions {
-    actions = [
-      "Microsoft.ContainerRegistry/registries/pull/read",
-      "Microsoft.ContainerRegistry/operations/read",
-      "Microsoft.ContainerRegistry/locations/operationResults/read",
-      "Microsoft.ContainerRegistry/registries/read",
-      "Microsoft.ContainerRegistry/registries/packages/archives/read",
-      "Microsoft.ContainerRegistry/registries/packages/archives/versions/read",
-      "Microsoft.ContainerRegistry/registries/push/write",
-      "Microsoft.ContainerRegistry/registries/runs/read",
-      "Microsoft.ContainerRegistry/registries/taskruns/read",
-      "Microsoft.ContainerRegistry/registries/tasks/read",
-      "Microsoft.ContainerRegistry/registries/taskruns/operationStatuses/read"
-    ]
-    not_actions = []
-  }
+#   permissions {
+#     actions = [
+#       "Microsoft.ContainerRegistry/registries/pull/read",
+#       "Microsoft.ContainerRegistry/operations/read",
+#       "Microsoft.ContainerRegistry/locations/operationResults/read",
+#       "Microsoft.ContainerRegistry/registries/read",
+#       "Microsoft.ContainerRegistry/registries/packages/archives/read",
+#       "Microsoft.ContainerRegistry/registries/packages/archives/versions/read",
+#       "Microsoft.ContainerRegistry/registries/push/write",
+#       "Microsoft.ContainerRegistry/registries/runs/read",
+#       "Microsoft.ContainerRegistry/registries/taskruns/read",
+#       "Microsoft.ContainerRegistry/registries/tasks/read",
+#       "Microsoft.ContainerRegistry/registries/taskruns/operationStatuses/read"
+#     ]
+#     not_actions = []
+#   }
 
-  assignable_scopes = [
-    azurerm_resource_group.app.id # scoped to resource group
-    # join("", ["/subscriptions/", var.subscription_id])
-  ]
-}
+#   assignable_scopes = [
+#     azurerm_resource_group.app.id # scoped to resource group
+#     # join("", ["/subscriptions/", var.subscription_id])
+#   ]
+# }
 
 
 resource "azurerm_role_assignment" "app" {
@@ -143,19 +143,19 @@ resource "azurerm_log_analytics_workspace" "app" {
 }
 
 
-resource "azurerm_container_app_environment" "app" {
-  name                       = var.project_name_long
-  location                   = var.azure_region
-  resource_group_name        = azurerm_resource_group.app.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.app.id
+# resource "azurerm_container_app_environment" "app" {
+#   name                       = var.project_name_long
+#   location                   = var.azure_region
+#   resource_group_name        = azurerm_resource_group.app.name
+#   log_analytics_workspace_id = azurerm_log_analytics_workspace.app.id
 
-  tags = {
-    project = var.project_name
-  }
-}
+#   tags = {
+#     project = var.project_name
+#   }
+# }
 
 
-# I was trying to add volume mounts, but they aren't supported by terraform.
+###### unused I was trying to add volume mounts, but they aren't supported by terraform.
 # resource "azurerm_virtual_network" "app" {
 #   name = var.project_name_long
 #   location = var.azure_region
@@ -229,7 +229,7 @@ resource "azurerm_container_app_environment" "app" {
 #   enabled_protocol = "NFS"
 #   # access_tier = "Hot"
 # }
-
+############ unused above this line
 
 # resource "azurerm_container_app" "app" {
 #   name                         = join("", [var.project_name, "app"])
